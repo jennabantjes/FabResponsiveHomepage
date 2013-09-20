4e1c24f2c1dec9edbdf6bcc75de15da2f0f2c75c
Jenna <jennabantjes@gmail.com>
2013-09-20 17:08:02 -0400

    Fix header

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -7,9 +7,35 @@
             console.log($width);
 
             $('#open-menu').click(function(){
-
                 $('ul.categories').slideToggle();
             });
+
+            $('ul.categories a').click(function(){
+                console.log('come on');
+                $('.icon-down-open').hide();
+                $('.icon-left-dir').show();
+            });
+
+            $('.icon-left-dir').click(function(){
+                $('ul.categories').hide();
+                $('.flyout').hide();
+                $('.icon-left-dir').hide();
+                $('.icon-down-open').show();
+            });
+
+            /*if ($('ul.categories').is(":hidden")) {
+                $('#open-menu').click(function(){
+                    console.log('default');
+                        $('ul.categories').slideToggle();
+                    });
+                }
+
+            else {
+                $('#open-menu').click(function(){
+                    console.log('visible');
+                    $('.arrow').removeClass('icon-down-open');
+                });
+            }*/
         }
 
     subNavs = function() {
